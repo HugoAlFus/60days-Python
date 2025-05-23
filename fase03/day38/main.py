72fb6dfb567f67a32574e7de2644ee5dd202ea56
HugoAlm-DAM <hugoalfus@gmail.com>
2025-05-23 07:15:43 +0200

    Day38 - Buscador libros Completado ✅

--- fase03/day38/main.py
+++ fase03/day38/main.py
@@ -11,14 +11,12 @@ def home():
     query = request.args.get('query')
     books = []
 
-    print(query)
     if query:
         url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             books = data.get('items', [])
-            print(books)
 
     return render_template('index.html', books=books, query=query)
 
